4f771c3e94d07dcdeaa9886c293ea4e8ad3f81b4
chungkh4 <chungkh4@gmail.com>
2026-04-29 12:27:44 +0800

    updated unit tests

--- FYP/src/test/java/TestRegisterServlet.java
+++ FYP/src/test/java/TestRegisterServlet.java
@@ -6,6 +6,8 @@ import com.auction.model.User;
 import com.auction.servlet.RegisterServlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,7 +15,7 @@ import org.mockito.Mockito;
 
 import java.io.*;
 
-
+@DisplayName("Testing RegisterServlet")
 public class TestRegisterServlet extends Mockito{
 
     private static class RegisterServletWrapper extends RegisterServlet{
@@ -23,178 +25,151 @@ public class TestRegisterServlet extends Mockito{
         }
     }
 
-    @Test
-    public void testInvalidEmail() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
+    private UserDAO mockDAO;
+    private RegisterServletWrapper mockServlet;
+    private HttpServletRequest mockRequest;
+    private HttpServletResponse mockResponse;
+    
+    @BeforeEach
+    public void setUp()
+    {
+        mockDAO = mock(UserDAO.class);
+        mockServlet = new RegisterServletWrapper();
         mockServlet.setUserDAO(mockDAO);
 
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("Test1");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("email");
-        when(request.getParameter("role")).thenReturn("buyer");
+        mockRequest = mock(HttpServletRequest.class);
+        mockResponse = mock(HttpServletResponse.class);
+    }
+    
+    @Test
+    @DisplayName("Testing invalid emails")
+    public void testInvalidEmail() throws Exception{
+        when(mockRequest.getParameter("username")).thenReturn("Test1");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("email");
+        when(mockRequest.getParameter("role")).thenReturn("buyer");
 
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), contains("Email"));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), contains("Email"));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 
     @Test
+    @DisplayName("Testing invalid username")
     public void testInvalidUsername() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn(" ");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("Test1@email.com");
-        when(request.getParameter("role")).thenReturn("buyer");
+        when(mockRequest.getParameter("username")).thenReturn(" ");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("Test1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("buyer");
 
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), eq("Username is required."));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), eq("Username is required."));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Password", "Password1", "Password@"})
+    @DisplayName("Testing invalid passwords")
     public void testPasswordValidation(String pass) throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("Test1");
-        when(request.getParameter("password")).thenReturn(pass);
-        when(request.getParameter("email")).thenReturn("Test1@email.com");
-        when(request.getParameter("role")).thenReturn("buyer");
+        when(mockRequest.getParameter("username")).thenReturn("Test1");
+        when(mockRequest.getParameter("password")).thenReturn(pass);
+        when(mockRequest.getParameter("email")).thenReturn("Test1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("buyer");
 
             //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), contains("Password"));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), contains("Password"));
         //assertTrue(stringWriter.toString().contains("Error"));
         }
 
     @Test
+    @DisplayName("Testing invalid roles")
     public void testInvalidRole() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("Test1");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("Test1@email.com");
-        when(request.getParameter("role")).thenReturn("");
+        when(mockRequest.getParameter("username")).thenReturn("Test1");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("Test1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("");
 
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), eq("Role is required."));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), eq("Role is required."));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 
     @Test
+    @DisplayName("Testing existing username")
     public void testExistingUsername() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
         when(mockDAO.checkUser("user1")).thenReturn(true);
 
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("user1");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("Test1@email.com");
-        when(request.getParameter("role")).thenReturn("seller");
+        when(mockRequest.getParameter("username")).thenReturn("user1");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("Test1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("seller");
 
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), eq("Username already in use!"));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), eq("Username already in use!"));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 
     @Test
+    @DisplayName("Testing existing email")
     public void testExistingEmail() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
         when(mockDAO.checkEmail("user1@email.com")).thenReturn(true);
 
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("Test1");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("user1@email.com");
-        when(request.getParameter("role")).thenReturn("seller");
+        when(mockRequest.getParameter("username")).thenReturn("Test1");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("user1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("seller");
 
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Error"), eq("Email already in use!"));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Error"), eq("Email already in use!"));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 
     @Test
+    @DisplayName("Testing insert")
     public void testInsert() throws Exception{
-        UserDAO mockDAO = mock(UserDAO.class);
+        when(mockRequest.getParameter("username")).thenReturn("Test2");
+        when(mockRequest.getParameter("password")).thenReturn("Password1!");
+        when(mockRequest.getParameter("email")).thenReturn("test1@email.com");
+        when(mockRequest.getParameter("role")).thenReturn("seller");
         when(mockDAO.insertUser(any(User.class))).thenReturn(true);
-
-        RegisterServletWrapper mockServlet = new RegisterServletWrapper();
-        mockServlet.setUserDAO(mockDAO);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getParameter("username")).thenReturn("Test1");
-        when(request.getParameter("password")).thenReturn("Password1!");
-        when(request.getParameter("email")).thenReturn("test1@email.com");
-        when(request.getParameter("role")).thenReturn("seller");
-
+        
         //until there's a response written in Servlet, check via verify()
 /*        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(printWriter);*/
 
-        mockServlet.doPost(request, response);
-        verify(request).setAttribute(eq("Insert"), eq("Insert ran!"));
+        mockServlet.doPost(mockRequest, mockResponse);
+        verify(mockRequest).setAttribute(eq("Insert"), eq("Insert ran!"));
         //assertTrue(stringWriter.toString().contains("Error"));
     }
 }
